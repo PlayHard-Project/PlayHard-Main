aaf2f177ba7225f5e7a98f2a1ff0163ab5afc501
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-11-07 20:24:58 -0400

    feat: Integration and implementation of MongoDB data base

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,3 +18,9 @@ app.use('/api', usersRoutes);
 app.get('/', (req, res) => {
     res.send("Welcome to Full API Rest Playhard E-commerce");
 });
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("-> Sucesfully connection to MongoDB Atlas."))
+.catch((error) => console.error(error));
+
+app.listen(port, () => console.log("-> Server is listening on port ", port));

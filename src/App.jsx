c37ed36c91fda26567c52d0f7e5c273e517099b8
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-11-07 22:06:31 -0400

    feat: Update the project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>DEVELOPMENT environment</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,5 +21,3 @@ function App() {
     </div>
   );
 }
-
-export default App;

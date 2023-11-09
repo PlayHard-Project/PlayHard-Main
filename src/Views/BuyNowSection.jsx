515e8d86c42d92c5b4897db3d818cb902ecc105a
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-11-09 04:12:00 -0400

    CU-8686a0p4v - ID - 2.2:  feat: Adding functionality to change slides automatically after 4 seconds using hooks

--- src/Views/BuyNowSection.jsx
+++ src/Views/BuyNowSection.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -7,13 +7,20 @@ import { data } from "../Components/Objects/CoverBuyNowSection";
 
 export default function BuyNowSection() {
   const sliderRef = useRef();
+  const [isTransitioning, setTransitioning] = useState(false);
 
   const sliderSettings = {
-    dots: false, // Configuración para ocultar los puntos de navegación
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    beforeChange: () => {
+      setTransitioning(true);
+    },
+    afterChange: () => {
+      setTransitioning(false);
+    },
   };
 
   const handlePrevClick = () => {
@@ -24,28 +31,47 @@ export default function BuyNowSection() {
     sliderRef.current.slickNext();
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      sliderRef.current.slickNext();
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section className="slider-container">
-      <Slider ref={sliderRef} {...sliderSettings}>
-        {data.map((item) => (
-          <div key={item.id} className="container-image">
-            <img
-              src={item.imgUrl}
-              alt={`CoverImg-${item.id}`}
-              className="responsive"
-            />
-          </div>
-        ))}
-      </Slider>
-      <div className="navigation-buttons">
-        <button className="navigation-button prev" onClick={handlePrevClick}>
-          Prev
-        </button>
-        <button className="navigation-button next" onClick={handleNextClick}>
-          Next
-        </button>
-      </div>
-      <button className="buy-now">Buy Now</button>
+      <section className="image-container">
+        <Slider ref={sliderRef} {...sliderSettings}>
+          {data.map((item) => (
+            <div key={item.id} className="container-image">
+              <img
+                src={item.imgUrl}
+                alt={`CoverImg-${item.id}`}
+                className="responsive"
+              />
+            </div>
+          ))}
+        </Slider>
+      </section>
+      <button className="navigation-button-prev" onClick={handlePrevClick}>
+        <img
+          className="arrow-icon"
+          src="https://i.postimg.cc/sD4Y649N/iconmonstr-arrow-64-240.png"
+          alt="Previous"
+        />
+      </button>
+      <button className="navigation-button-next" onClick={handleNextClick}>
+        <img
+          className="arrow-icon"
+          src="https://i.postimg.cc/mgZQ01m2/iconmonstr-arrow-63-240.png"
+          alt="Previous"
+        />
+      </button>
+
+      <button className={`buy-now ${isTransitioning ? "hidden" : ""}`}>
+        Buy Now
+      </button>
     </section>
   );
 }

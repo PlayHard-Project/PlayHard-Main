88275777192bf35e1344bdf6402172a83abc4960
aguirrekarina <karina.aguirre.9491656@usalesiana.edu.bo>
2023-11-08 23:50:42 -0400

    Implementation header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-export default function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Views/Header.jsx';
+import Home from './Views/Home';
+import Shop from './Views/Shop';
+import About from './Views/About';
+import Pages from './Views/Pages';
+import Contact from './Views/Contact';
+
+function App() {
     return (
-        <h1 className="text-3xl font-bold underline hover:text-5xl">
-            Hello world!
-        </h1>
-    )
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/shop" element={<Shop />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/pages" element={<Pages />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </Router>
+    );
 }
+
+export default App;
---
+++ src/Views/Header.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import '../css/headerStyle.css';
+import headerIcon from "../Images/header-icon.png";
+import cartIcon from "../Images/cart-icon.svg";
+import menuIcon from "../Images/menu-icon.svg";
+import personIcon from "../Images/person-icon.svg";
+import searchIcon from "../Images/search-icon.svg";
+import settingsIcon from "../Images/settings-icon.svg";
+import closeIcon from "../Images/close-icon.svg"
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    const [showSearchPopup, setShowSearchPopup] = useState(false);
+    const [showMenuPopup, setShowMenuPopup] = useState(false);
+
+    const toggleSearchPopup = () => {
+        setShowSearchPopup(!showSearchPopup);
+        setShowMenuPopup(false);
+    };
+
+    const toggleMenu = () => {
+        setShowMenuPopup(!showMenuPopup);
+        setShowSearchPopup(false);
+    };
+
+    return (
+        <header className="p-4 text-white header">
+            <div className="flex justify-between items-center">
+                <div className="flex items-center space-x-4">
+                    <img src={headerIcon} alt="Icon Main" className="text-2xl font-bold background-shape" />
+                    <div className="md:flex space-x-4 hidden ">
+                        <Link to="/home"> <div className="text" style={{ marginLeft: '30px' }}>Home</div> </Link>
+                        <Link to="/shop"> <div className="text" style={{ marginLeft: '10px' }}>Shop</div> </Link>
+                        <Link to="/about"> <div className="text" style={{ marginLeft: '10px' }}>About</div> </Link>
+                        <Link to="/pages"> <div className="text" style={{ marginLeft: '10px' }}>Pages</div> </Link>
+                        <Link to="/contact"> <div className="text" style={{ marginLeft: '10px' }}>Contact</div> </Link>
+                    </div>
+                </div>
+                <div className="md:flex hidden space-x-4 items-center">
+                    <div className="flex items-center search-container">
+                        <input type="text" className="search-input" placeholder="Search" />
+                        <button>
+                            <img src={searchIcon} alt="Close Icon" className="w-6 h-6 rounded-full" />
+                        </button>
+                    </div>
+                    <div className="md:flex hidden">
+                        <img src={personIcon} alt="Person Icon" className="w-10 h-10 rounded-full" />
+                    </div>
+                    <button className="text md:flex hidden">Login</button>
+                    <div className="text-black md:flex hidden">/</div>
+                    <button className="text md:flex hidden">Register</button>
+                    <div className="relative md:flex hidden">
+                        <img src={cartIcon} alt="Cart Icon" className="style-icon" />
+                        <span className="bg-red-500 text-white absolute top-0 right-0 w-4 h-4 flex
+                        items-center justify-center rounded-full"> 0 </span>
+                    </div>
+                    <div className="md:flex hidden">
+                        <img src={settingsIcon} alt="Settings Icon" className="style-icon" />
+                    </div>
+                </div>
+                <div className="md:hidden space-x-4 relative">
+                    <div className="flex items-center space-x-4">
+                        <div onClick={toggleSearchPopup}>
+                            <img src={searchIcon} alt="Search Icon" className="style-icon" />
+                        </div>
+                        <button onClick={toggleMenu}>
+                            <img src={menuIcon} alt="Menu Icon" className="style-icon text-white" />
+                        </button>
+                    </div>
+                    {showSearchPopup && (
+                        <div className="absolute shadow-lg popup right-0 search-container-little">
+                            <input type="text" className="search-input-little" placeholder="Search" />
+                            <button onClick={toggleSearchPopup}>
+                                <img src={closeIcon} alt="Close Icon" className="w-7 h-6 rounded-full" />
+                            </button>
+                        </div>
+                    )}
+                    {showMenuPopup && (
+                        <div className="absolute p-2 shadow-lg popup right-0">
+                            <Link to="/home"> <div className="text">Home</div> </Link>
+                            <Link to="/shop"> <div className="text">Shop</div> </Link>
+                            <Link to="/about"> <div className="text">About</div> </Link>
+                            <Link to="/pages"> <div className="text">Pages</div> </Link>
+                            <Link to="/contact"> <div className="text">Contact</div> </Link>
+                            <div className="text">Login / Register</div>
+                            <div className="relative flex items-center">
+                                <img src={cartIcon} alt="Cart Icon" className="style-icon" />
+                                <div className="text">Shop Cart</div>
+                            </div>
+                            <div className="relative flex items-center">
+                                <img src={settingsIcon} alt="Settings Icon" className="style-icon" />
+                                <div className="text">Settings</div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;

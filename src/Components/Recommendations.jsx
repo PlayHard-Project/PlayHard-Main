9097ec80eb730ab65d691ea211a435361c455159
JuanPabloArequipa <juan.arequipa.12833847@usalesiana.edu.bo>
2023-11-09 19:00:26 -0400

    CU-8686a0qh5 Change to responsive

--- src/Components/Recommendations.jsx
+++ src/Components/Recommendations.jsx
@@ -14,13 +14,40 @@ export default function Recommendations() {
     infinite: true,
     speed: 800,
     slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToScroll: 4,
     beforeChange: () => {
       setTransitioning(true);
     },
     afterChange: () => {
       setTransitioning(false);
     },
+    responsive: [
+      {
+        breakpoint: 1424,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+
+      {
+        breakpoint: 1124,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+    ],
   };
 
   const handlePrevClick = () => {
@@ -42,27 +69,31 @@ export default function Recommendations() {
   return (
     <section className="slider-container">
       <section className="image-container">
+        <h1 className="main-title"> RECOMMENDED </h1>
         <Slider ref={sliderRef} {...sliderSettings}>
           {products?.map((item, index) => {
             return <BasicCard item={item} />;
           })}
         </Slider>
       </section>
-      {!isTransitioning}
-      <button className="navigation-button-prev" onClick={handlePrevClick}>
-        <img
-          className="arrow-icon"
-          src="https://i.postimg.cc/sD4Y649N/iconmonstr-arrow-64-240.png"
-          alt="Previous"
-        />
-      </button>
-      <button className="navigation-button-next" onClick={handleNextClick}>
-        <img
-          className="arrow-icon"
-          src="https://i.postimg.cc/mgZQ01m2/iconmonstr-arrow-63-240.png"
-          alt="Previous"
-        />
-      </button>
+      {!isTransitioning && (
+      <>
+        <button className="navigation-button-prev" onClick={handlePrevClick}>
+          <img
+            className="arrow-icon"
+            src="https://i.postimg.cc/sD4Y649N/iconmonstr-arrow-64-240.png"
+            alt="Previous"
+          />
+        </button>
+        <button className="navigation-button-next" onClick={handleNextClick}>
+          <img
+            className="arrow-icon"
+            src="https://i.postimg.cc/mgZQ01m2/iconmonstr-arrow-63-240.png"
+            alt="Next"
+          />
+        </button>
+      </>
+    )}
     </section>
   );
 }

25e0b64f46c3ab46689d9e1e1bc516fa035ffcb1
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-11-09 04:09:06 -0400

    CU-8686a0p4v - ID - 2.2:  feat: Implementation routating covers in a react component using a module of react to develop easier that part

--- src/Views/BuyNowSection.jsx
+++ src/Views/BuyNowSection.jsx
@@ -1,27 +1,32 @@
 import React, { useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./BuyNowSection.css";
 import { data } from "../Components/Objects/CoverBuyNowSection";
 
 export default function BuyNowSection() {
-  const listRefCovers = useRef();
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <section className="slider-container">
-      <div className="container-image">
-        <ul ref={listRefCovers}>
-            {
-                data.map((item) => {
-                    return <li key ={item.id}>
-                        <img src={item.imgUrl} alt="img" /> 
-                    </li>
-                })
-            }
-        </ul>
-        <img
-          src="https://i.postimg.cc/vZLC88NT/Group-100-1.png"
-          alt="CoverImg"
-          className="responsive"
-        />
-      </div>
+      <Slider {...sliderSettings}>
+        {data.map((item) => (
+          <div key={item.id} className="container-image">
+            <img
+              src={item.imgUrl}
+              alt={`CoverImg-${item.id}`}
+              className="responsive"
+            />
+          </div>
+        ))}
+      </Slider>
       <button className="buy-now">Buy Now</button>
     </section>
   );

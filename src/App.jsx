c8763aa0f586248f52b53bb3c03ef2ada4d75d30
DiegoFigueroaSevillano <diego.figueroa.8032745@usalesiana.edu.bo>
2023-11-09 18:43:36 -0400

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import { ProductSection } from "./Pages/ProductSection";
+import Home from "./Pages/Home/Home";
 
-
-{/* BORRAR ESTE COMPONENTE, SOLO ESTA PARA EL TESTEO*/}
-function ProductButtons() {
-    const productIds = ['654c436360c78adccb61fc21', '654c436360c78adccb61fc42', '654c436360c78adccb61fbec'];
-
-    return (
-        <div className={'flex flex-col items-center text-center justify-center max-w-7xl'}>
-            {productIds.map(id => (
-                <Link key={id} to={`/product/${id}`}>
-                    <button>Ir a producto {id}</button>
-                </Link>
-            ))}
-        </div>
-    );
-}
 
 export default function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<ProductButtons />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/product/:id" element={<ProductSection />} />
             </Routes>
         </Router>

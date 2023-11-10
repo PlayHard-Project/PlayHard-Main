a17474adf20a10b3ea17ac760ef21915e691303e
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-11-09 21:32:08 -0400

    fix: Merge conflicts solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,29 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './Views/Header.jsx';
-import Home from './Views/Home';
-import Shop from './Views/Shop';
-import About from './Views/About';
-import Pages from './Views/Pages';
-import Contact from './Views/Contact';
+import React, { useState } from 'react';
+import Footer from './Views/BaseViews/Footer.jsx';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { ProductSection } from "./Views/ProductDescriptionSection/ProductSection.jsx";
+import Home from "./Views/Pages/Home.jsx";
+import Header from './Views/BaseViews/Header.jsx';
+import Shop from './Views/Pages/Shop.jsx';
+import About from './Views/Pages/About.jsx';
+import Pages from './Views/Pages/Pages.jsx';
+import Contact from './Views/Pages/Contact.jsx';
 
-function App() {
+
+export default function App() {
     return (
         <Router>
-            <Header />
+            <Header/>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/product/:id" element={<ProductSection />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/shop" element={<Shop />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/pages" element={<Pages />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
-        </Router>
-    );
-}
-
-export default App;
-=======
-import React, { useState } from 'react';
-import Footer from './Views/Footer/Footer';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import { ProductSection } from "./Pages/ProductSection";
-import Home from "./Pages/Home/Home";
-
-
-export default function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/product/:id" element={<ProductSection />} />
-            </Routes>
             <Footer />
         </Router>
     );
 }
->>>>>>> develop

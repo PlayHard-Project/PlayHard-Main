909d32fe178e54dfd831d4e4a149cf2e39e2efb2
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-11-09 21:32:40 -0400

    fix: Updating the folders and the structure of project

---
+++ src/Views/Pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className= "container">
+            <h1>About Page</h1>
+            {/* Content About us */}
+        </div>
+    );
+};
+
+export default About;

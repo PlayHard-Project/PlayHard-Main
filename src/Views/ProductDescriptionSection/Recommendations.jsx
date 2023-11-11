ae5a602ddf8a8c67e7bd88213ea7fb775b4afd06
JuanPabloArequipa <juan.arequipa.12833847@usalesiana.edu.bo>
2023-11-10 22:42:40 -0400

    CU-8686a0qh5 Screen sizes fix

--- src/Views/ProductDescriptionSection/Recommendations.jsx
+++ src/Views/ProductDescriptionSection/Recommendations.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef} from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -8,61 +8,50 @@ import { data } from "../../Components/Objects/RecommendationsContent";
 
 export default function Recommendations() {
   const sliderRef = useRef();
-  const [isTransitioning, setTransitioning] = useState(false);
 
   const sliderSettings = {
     dots: false,
     infinite: true,
     speed: 800,
     slidesToShow: 4,
-    slidesToScroll: 4,
-    beforeChange: () => {
-      setTransitioning(true);
-    },
-    afterChange: () => {
-      setTransitioning(false);
-    },
+    slidesToScroll: 1,
+    swipeToSlide: true,
     responsive: [
       {
-        breakpoint: 1424,
+        breakpoint: 1450,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
+          swipeToSlide: true,
         },
       },
 
       {
-        breakpoint: 1124,
+        breakpoint: 1300,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
           infinite: true,
+          swipeToSlide: true,
         },
       },
       {
-        breakpoint: 800,
+        breakpoint: 650,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          initialSlide: 2,
+          infinite: true,
+          swipeToSlide: true,
         },
       },
     ],
   };
 
-  const handlePrevClick = () => {
-    sliderRef.current.slickPrev();
-  };
-
-  const handleNextClick = () => {
-    sliderRef.current.slickNext();
-  };
-
   useEffect(() => {
     const interval = setInterval(() => {
       sliderRef.current.slickNext();
-    }, 6000);
+    }, 5000);
 
     return () => clearInterval(interval);
   }, []);
@@ -77,24 +66,6 @@ export default function Recommendations() {
           })}
         </Slider>
       </section>
-      {!isTransitioning && (
-      <>
-        <button className="navigation-button-prev" onClick={handlePrevClick}>
-          <img
-            className="arrow-icon"
-            src="https://i.postimg.cc/sD4Y649N/iconmonstr-arrow-64-240.png"
-            alt="Previous"
-          />
-        </button>
-        <button className="navigation-button-next" onClick={handleNextClick}>
-          <img
-            className="arrow-icon"
-            src="https://i.postimg.cc/mgZQ01m2/iconmonstr-arrow-63-240.png"
-            alt="Next"
-          />
-        </button>
-      </>
-    )}
     </section>
   );
 }

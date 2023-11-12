9ba62c8ad1a01e63b0f724da984d75cb18e8644c
JuanPabloArequipa <juan.arequipa.12833847@usalesiana.edu.bo>
2023-11-12 15:30:13 -0400

    cu 8686a0qh5 Fixing container

--- src/Views/ProductDescriptionSection/Recommendations.jsx
+++ src/Views/ProductDescriptionSection/Recommendations.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef} from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./Recommendations.css";
+import "../../css/Recommendations.css";
 import BasicCard from "./Card";
 import { data } from "../../Components/Objects/RecommendationsContent";
 

433f9672825206f1a32a2f9ea327f46b098d6fde
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-11-11 20:26:48 -0400

    feat: Adding Schema and route for offers

---
+++ backend/src/models/offerSchema.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const offerSchema = new Schema({
+  name: {
+    type: String,
+    required: false,
+  },
+  imagePath: {
+    type: String,
+    required: false,
+  },
+  reactRoute: {
+    type: String,
+    required: false,
+  },
+  idProduct: {
+    type: String,
+    required: false,
+    ref: 'Product'
+  },
+});
+
+module.exports = mongoose.model("Offer", brandSchema);

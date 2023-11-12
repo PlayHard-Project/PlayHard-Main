0b3bef830069fc498622eca06481f2ff968c16dc
JuanPabloArequipa <juan.arequipa.12833847@usalesiana.edu.bo>
2023-11-12 14:46:30 -0400

    cu 8686a0qh5 Fixing comments

--- src/Views/ProductDescriptionSection/Card.jsx
+++ src/Views/ProductDescriptionSection/Card.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
+import '../../css/Card.css';
 
 function CustomCard({ item }) {
   return (
-    <button
-      style={{
-        width: '300px',
-        height: '300px',
-        border: '1px solid rgb(49, 54, 61)',
-        backgroundColor: '#fff',
-        color: '#000',
-        textIndent: '10px',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      }}
-    >
+    <button className="custom-card">
       <img
         src={item?.image}
         alt={item?.name}
-        style={{
-          width: '100%',
-          height: '70%',
-          objectFit: 'cover',
-          borderTopLeftRadius: '4px',
-          borderTopRightRadius: '4px',
-        }}
+        className="card-image"
       />
-      <div style={{ padding: '5%', textAlign: "left" }}>
-        <h3 style={{ marginBottom: '8px' }}>{item.name}</h3>
-        <p>{item.price}</p>
+      <div className="card-content">
+        <h3 className="card-title">{item.name}</h3>
+        <p className="card-price">{item.price}</p>
       </div>
     </button>
   );

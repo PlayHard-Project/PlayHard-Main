ef8dbb34265c09dec9960abdd30958438078d3b6
aguirrekarina <karina.aguirre.9491656@usalesiana.edu.bo>
2023-11-12 14:04:27 -0400

    fix icons

--- src/Components/Objects/CoverBuyNowSection.js
+++ src/Components/Objects/CoverBuyNowSection.js
@@ -1,18 +1,18 @@
 export const data = [
     {
         id: 1,
-        imgUrl: "https://i.postimg.cc/ydQKM3J3/Group-108.png"
+        imgUrl: "https://res.cloudinary.com/playhardimages/image/upload/v1699733883/Cover/Group-108.png"
     },
     {
         id: 2,
-        imgUrl: "https://i.postimg.cc/C1LYrsKV/Group-109.png"
+        imgUrl: "https://res.cloudinary.com/playhardimages/image/upload/v1699733885/Cover/Group-109.png"
     },
     {
         id: 3,
-        imgUrl: "https://i.postimg.cc/R0T4TD4R/Group-110.png"
+        imgUrl: "https://res.cloudinary.com/playhardimages/image/upload/v1699733883/Cover/Group-110.png"
     },
     {
         id: 4,
-        imgUrl: "https://i.postimg.cc/xdQ9M2D8/Group-111.png"
+        imgUrl: "https://res.cloudinary.com/playhardimages/image/upload/v1699733883/Cover/Group-111.png"
     }
 ]

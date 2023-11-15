a9d43dda7f2d75532e483a75ceb2451de0c5757d
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-11-14 21:22:27 -0400

    feat: Adding the routes and finish the implementation

--- backend/src/serverStripe.js
+++ backend/src/serverStripe.js
@@ -1,16 +1,23 @@
 require('dotenv').config();
 
 const express = require('express');
+const stripe = require('stripe');
 
 const configureAppImplementingStrypeServer = (app) => {
     
     app.use(express.static('public'));
     app.use(express.json());
 
+    let stripeGateway = stripe(process.env.STRIPE_API_URI);
+
     app.get('/', (req, res) => {
         res.send("Welcome to Full API Rest Playhard E-commercessss");
     });
     
+    app.post('/strype-api/intent-payment');
+    app.post('/strype-api/confirm-payment');
+    app.post('/strype-api/failure-payment');
+
     console.log("-> Successfully connected to Stripe server.")
 };
 

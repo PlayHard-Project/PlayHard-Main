7bf08db1cbfb394776ba2f44441554f3816e121f
SamuelEscalera <samuel.escalera.9413603@usalesiana.edu.bo>
2023-11-14 22:54:28 -0400

    categorie header final design

--- src/Views/HomePageSections/CategoriesHeaderSection.jsx
+++ src/Views/HomePageSections/CategoriesHeaderSection.jsx
@@ -1,23 +1,43 @@
 import React from "react";
 import "../../css/categoryHeaderSection.css"
+import { IoIosArrowDown } from "react-icons/io";
 
 const CategoriesHeaderSection = () => {
 
     return (
         <div className="CategoriesHeaderConteiner"> 
             <div>
-                <h1>CLOTHES</h1>
+                <h1>
+                    <button>
+                    <span> CLOTHES</span>
+                    <i class="fas fa-star">
+                        <IoIosArrowDown></IoIosArrowDown>
+                    </i>
+                    </button>
+                </h1>
                 <h1>SHOES</h1>
                 <h1>EQUIPMENT</h1>
                 <h1>ACCESSORIES</h1>
-                <h1>BRANDS</h1>
+                <h1>
+                <button>
+                    <span> BRANDS</span>
+                    <i class="fas fa-star">
+                        <IoIosArrowDown></IoIosArrowDown>
+                    </i>
+                    </button>
+                </h1>
                 <h1>OFFERS</h1>
-                <h1>SPORTS</h1>
+                <h1>
+                <button>
+                    <span> CLOTHES</span>
+                    <i class="fas fa-star">
+                        <IoIosArrowDown></IoIosArrowDown>
+                    </i>
+                    </button>
+                </h1>
             </div>
         </div>
     )
-
-    
 
 }
 

1f29ef35129e4c5c0c7c1c876aa350373ec8c7ae
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-11-16 09:02:07 -0400

    feat: Adding component for button to go to checkout

--- src/Views/Pages/DeleteButtonsPayments.jsx
+++ src/Views/Pages/DeleteButtonsPayments.jsx
@@ -1,86 +1,10 @@
 import { Link } from "react-router-dom";
-import { loadStripe } from "@stripe/stripe-js";
-import { getElementByID } from "../../Components/ApiRestHandler/requestHandler";
-import { useEffect, useState } from "react";
-import BuyCartManagement from "../../Utilities/BuyCartManagement";
-import ProductEntityForStripe from "../../Entities/ProductEntityForStripe";
+import GoToCheckout  from "../../Components/ApiRestHandler/stripeApiHandler"
 
 {
   /* BORRAR ESTE COMPONENTE, SOLO ESTA PARA EL TESTEO*/
 }
 export function ProductButtonsD() {
-  const manager = new BuyCartManagement();
-  const productsArray = manager.getProducts();
-
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const updatedProducts = await Promise.all(
-        productsArray.map(async (product) => {
-          const productFromAPI = await getElementByID(product.id, "products");
-          const productToSendTheServer = new ProductEntityForStripe(
-            product.id,
-            productFromAPI.name,
-            Math.round(productFromAPI.price * 100),
-            product.quantity,
-            productFromAPI.description,
-            productFromAPI.imagePath
-          );
-          console.log(productToSendTheServer);
-          return productToSendTheServer;
-        })
-      );
-
-      setProducts(updatedProducts);
-      console.log(updatedProducts + " CDFGERGRGTR");
-    };
-
-    fetchData();
-  }, []);
-
-  const makePayment = async () => {
-    const stripe = await loadStripe(
-      "pk_test_51OCX2QHsWC39RHnvTHY4jNmDT18JHg9Vh1s0aJmuDtMPPzS4mjcOMU5gvO4Yj6mvPpGQ9yNFjEnxPx0ecl2c6QKo00xIEzm1lX"
-    );
-
-    const body = {
-      products: products,
-    };
-
-    const headers = {
-      "Content-Type": "application/json",
-    };
-
-    try {
-      const response = await fetch(
-        "http://localhost:9000/stripe-api/intent-payment",
-        {
-          method: "POST",
-          headers: headers,
-          body: JSON.stringify(body),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error(`Server returned status ${response.status}`);
-      }
-
-      const session = await response.json();
-
-      console.log("Session ID:", session.id);
-
-      const result = stripe.redirectToCheckout({
-        sessionId: session.id,
-      });
-
-      if (result.error) {
-        console.log(result.error);
-      }
-    } catch (error) {
-      console.error("Error making payment:", error);
-    }
-  };
-
   return (
     <div
       className={
@@ -95,7 +19,7 @@ export function ProductButtonsD() {
           Ir al estado de pago Successful
         </button>
       </Link>
-      <button onClick={makePayment}>Checkout</button>
+      <GoToCheckout />
     </div>
   );
 }

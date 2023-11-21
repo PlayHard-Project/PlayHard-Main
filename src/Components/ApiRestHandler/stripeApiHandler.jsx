680ea2b36efc423169a7d87c71a73e8a646bab97
aguirrekarina <karina.aguirre.9491656@usalesiana.edu.bo>
2023-11-20 22:58:15 -0400

    changes of categories view

--- src/Components/ApiRestHandler/stripeApiHandler.jsx
+++ src/Components/ApiRestHandler/stripeApiHandler.jsx
@@ -5,26 +5,28 @@ import BuyCartManagement from "../../Utilities/BuyCartManagement";
 import ProductEntityForStripe from "../../Entities/ProductEntityForStripe";
 import '../../css/CartShop.css';
 
-export default function GoToCheckout() {
+// Functional component for the checkout button
+export default function GoToCheckout({ disabled }) {
   const [products, setProducts] = useState([]);
 
+  // Fetch products and format them when the component mounts
   useEffect(() => {
     const fetchData = async () => {
       const productsArray = await new BuyCartManagement().getProducts();
 
       const updatedProducts = await Promise.all(
-        productsArray.map(async (product) => {
-          const productFromAPI = await getElementByID(product.id, "products");
-          const productToSendTheServer = new ProductEntityForStripe(
-            product.id,
-            productFromAPI.name,
-            Math.round(productFromAPI.price * 100),
-            product.quantity,
-            productFromAPI.description,
-            productFromAPI.imagePath
-          );
-          return productToSendTheServer;
-        })
+          productsArray.map(async (product) => {
+            const productFromAPI = await getElementByID(product.id, "products");
+            const productToSendTheServer = new ProductEntityForStripe(
+                product.id,
+                productFromAPI.name,
+                Math.round(productFromAPI.price * 100),
+                product.quantity,
+                productFromAPI.description,
+                productFromAPI.imagePath
+            );
+            return productToSendTheServer;
+          })
       );
 
       setProducts(updatedProducts);
@@ -32,22 +34,23 @@ export default function GoToCheckout() {
     fetchData();
   }, []);
 
+  // Function to handle the payment process
   const makePayment = async () => {
     try {
       const stripe = await loadStripe(
-        "pk_test_51OCX2QHsWC39RHnvTHY4jNmDT18JHg9Vh1s0aJmuDtMPPzS4mjcOMU5gvO4Yj6mvPpGQ9yNFjEnxPx0ecl2c6QKo00xIEzm1lX"
+          "pk_test_51OCX2QHsWC39RHnvTHY4jNmDT18JHg9Vh1s0aJmuDtMPPzS4mjcOMU5gvO4Yj6mvPpGQ9yNFjEnxPx0ecl2c6QKo00xIEzm1lX"
       );
 
       const body = { products: products };
       const headers = { "Content-Type": "application/json" };
 
       const response = await fetch(
-        "https://backend-fullapirest-test.onrender.com/stripe-api/intent-payment",
-        {
-          method: "POST",
-          headers: headers,
-          body: JSON.stringify(body),
-        }
+          "https://backend-fullapirest-test.onrender.com/stripe-api/intent-payment",
+          {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(body),
+          }
       );
 
       if (!response.ok) {
@@ -65,5 +68,14 @@ export default function GoToCheckout() {
     }
   };
 
-  return <button className="checkout-button" onClick={makePayment}>Checkout</button>;
+  // Render the checkout button
+  return (
+      <button
+          className={`checkout-button ${disabled ? "disabled" : ""}`}
+          onClick={makePayment}
+          disabled={disabled}
+      >
+        Checkout
+      </button>
+  );
 }

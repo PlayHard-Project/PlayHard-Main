bdc5f2c76716ca5e61ce3469fe630a9c51654f19
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-11-21 00:51:37 -0400

    feat: Reception of data and proccesing  done

--- backend/src/serverStripe.js
+++ backend/src/serverStripe.js
@@ -20,20 +20,6 @@ const configureAppImplementingStripeServer = (app) => {
   app.use(express.static("public"));
   app.use(express.json());
   app.use(cors());
-  
-  app.use("/api/Stripe");
-  
-  /**
-   * Root endpoint that sends a welcome message.
-   * @name app.get
-   * @method
-   * @param {string} '/' - The root path.
-   * @param {Function} (req, res) - Callback function to handle the route.
-   * @returns {void}
-   */
-  app.get("/", (req, res) => {
-    res.send("Welcome to Full API Rest Playhard E-commerce");
-  });
 
   /**
    * Endpoint to create a payment session for Stripe Checkout.
@@ -44,9 +30,14 @@ const configureAppImplementingStripeServer = (app) => {
    * @returns {void}
    */
   app.post("/stripe-api/intent-payment", async (req, res) => {
-    const { products } = req.body;
+    const customer = await stripeGateway.customers.create({
+      metadata: {
+        userId: req.body.userId,
+        products: JSON.stringify(req.body.products),
+      },
+    });
 
-    const lineItems = products.map((product) => ({
+    const lineItems = req.body.products.map((product) => ({
       price_data: {
         currency: "usd",
         product_data: {
@@ -63,47 +54,86 @@ const configureAppImplementingStripeServer = (app) => {
       const session = await stripeGateway.checkout.sessions.create({
         payment_method_types: ["card"],
         mode: "payment",
+        customer: customer.id,
         line_items: lineItems,
         success_url: "https://play-hard-main.vercel.app/success-payment-status",
         cancel_url: "https://play-hard-main.vercel.app/fail-payment-status",
         billing_address_collection: "required",
       });
       res.json({ id: session.id });
-
-     
-      // This is your Stripe CLI webhook secret for testing your endpoint locally.
-      const endpointSecret =
-        "whsec_ee6987fdc0111b98a550260fc1735f36d93d72b56647b3e9593af2b4ac72c71d";
-
-      app.post(
-        "/webhook",
-        express.raw({ type: "application/json" }),
-        (request, response) => {
-          const sig = request.headers["stripe-signature"];
-
-          let event;
-
-          try {
-            event = stripe.webhooks.constructEvent(
-              request.body,
-              sig,
-              endpointSecret
-            );
-            console.log("Webhook verified.")
-          } catch (err) {
-            console.log(`Webhook Error: ${err.message}`);
-            response.status(400).send(`Webhook Error: ${err.message}`);
-            return;
-          }
-
-          // Return a 200 response to acknowledge receipt of the event
-          response.send().end();
-        }
-      );
     } catch (error) {
       console.error("Error creating payment session:", error);
       res.status(500).json({ error: "Error creating payment session" });
     }
+  });
+
+  // This is your Stripe CLI webhook secret for testing your endpoint locally.
+  let endpointSecret;
+
+  //   endpointSecret =
+  //   "whsec_ee6987fdc0111b98a550260fc1735f36d93d72b56647b3e9593af2b4ac72c71d";
+
+  app.post(
+    "/stripe-api/webhook",
+    express.raw({ type: "application/json" }),
+    (request, response) => {
+      const sig = request.headers["stripe-signature"];
+
+      let data;
+      let eventType;
+
+      if (endpointSecret) {
+        let event;
+
+        try {
+          event = stripe.webhooks.constructEvent(
+            request.body,
+            sig,
+            endpointSecret
+          );
+          console.log("Webhook verified.");
+        } catch (err) {
+          console.log(`Webhook Error: ${err.message}`);
+          response.status(400).send(`Webhook Error: ${err.message}`);
+          return;
+        }
+
+        data = event.data.Object;
+        eventType = event.type;
+      } else {
+        data = request.body.data.Object;
+        eventType = request.body.type;
+      }
+
+      console.log("Webhook data:", data);
+      console.log("Webhook eventType:", eventType);
+
+      if (eventType === "checkout.session.completed") {
+        stripeGateway.customers
+          .retrieve(data.customer)
+          .then((customer) => {
+            console.log(customer);
+            console.log("data: ", data);
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+      }
+      // Return a 200 response to acknowledge receipt of the event
+      response.send().end();
+    }
+  );
+
+  /**
+   * Endpoint for testing purposes (placeholder for future implementation).
+   * @name app.get
+   * @method
+   * @param {string} '/stripe-api/intent-payment' - The path for testing intent payment.
+   * @param {Function} (req, res) - Callback function to handle the route.
+   * @returns {void}
+   */
+  app.get("/stripe-api/intent-payment", (req, res) => {
+    res.send("Intent-payment");
   });
 
   /**

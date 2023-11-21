3a30382958b20ed6346ccc48644c0ff3070e060a
SamuelEscalera <samuel.escalera.9413603@usalesiana.edu.bo>
2023-11-21 08:59:48 -0400

    correct conection POST

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const userSchema = require('../models/userSchema');
 const createRoutes = require('./routeHandler');
-const {registerUser} = require('../controllers/userController')
+// const {registerUser} = require('./userHandler')
 
 const router = express.Router();
 
 createRoutes(router, userSchema, 'users');
 
-router.post('/register', registerUser)
+//router.post('/register', registerUser)
+
 module.exports = router;

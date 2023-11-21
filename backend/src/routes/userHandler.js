8126c06e109fa4d3b4ccbe5249cdf7b48fc04150
SamuelEscalera <samuel.escalera.9413603@usalesiana.edu.bo>
2023-11-21 10:11:21 -0400

    error handling

--- backend/src/routes/userHandler.js
+++ backend/src/routes/userHandler.js
@@ -1,39 +1,49 @@
 const User = require('../models/userSchema')
 
 
-const registerUser = async (req, res) => {
-    try {
-        const {name, email, password} = req.body;
-        // Check name
-        if (!name) {
-            return res.json({
-                error: 'name is required'
+function createRoutes (router, model, baseRoute) {
+    /**
+     * Endpoint to create a new item in the database.
+     * @name router.post
+     * @method
+     * @param {string} `/${baseRoute}` - The path for creating a new item.
+     * @param {Function} (req, res) - Callback function to handle the route.
+     * @returns {void}
+     */
+    router.post(`/${baseRoute}`, async (req, res) => {
+        try {
+            const {name, email, password} = req.body;
+            // Check name
+            if (!name) {
+                return res.json({
+                    error: 'name is required'
+                })
+            };
+            // Check email
+            const exist = await User.findOne({email});
+            if (exist) {
+                return res.json({
+                    error: 'Email is already register'
+                })
+            };
+            // Check password
+            if (!password || password.lenght < 6){
+                return res.json({
+                    error: 'Password is required and should be at least 6 characters laong'
+                })
+            };
+    
+            const user = await User.create({
+                name, email, password
             })
-        };
-        // Check email
-        const exist = await User.findOne({email});
-        if (exist) {
-            return res.json({
-                error: 'Email is already register'
-            })
-        };
-        // Check password
-        if (!password || password.lenght < 6){
-            return res.json({
-                error: 'Password is required and should be at least 6 characters laong'
-            })
-        };
-
-        const user = await User.create({
-            name, email, password
-        })
-
-        return res.jason(user)
-    } catch (error) {
-        console.log(error)
-    }
-}
+    
+            return res.jason(user)
+        } catch (error) {
+            console.log(error)
+        }
+    });
+}  
 
 module.exports = {
-    registerUser
+    createRoutes
 }
--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const userSchema = require('../models/userSchema');
-const createRoutes = require('./routeHandler');
-// const {registerUser} = require('./userHandler')
+//const createRoutes = require('./routeHandler');
+const {createRoutes} = require('./userHandler')
 
 const router = express.Router();
 

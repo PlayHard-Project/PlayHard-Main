b07aea8d5e524f10ac4518a3876291fefeeb89ea
SamuelEscalera <samuel.escalera.9413603@usalesiana.edu.bo>
2023-11-22 09:07:02 -0400

    Documentation

--- backend/src/models/userSchema.js
+++ backend/src/models/userSchema.js
@@ -1,20 +1,61 @@
+/**
+ * @file Defines the mongoose schema and model for the 'User' collection.
+ * @module models/user
+ */
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+/**
+ * @typedef {Object} User
+ * @property {string} name - The name of the user.
+ * @property {string} email - The email address of the user.
+ * @property {string} password - The password of the user.
+ * @property {boolean} isAdmin - A flag indicating whether the user is an admin.
+ */
+
+/**
+ * Represents the mongoose schema for the 'User' collection.
+ * @type {mongoose.Schema<User>}
+ */
 const userSchema = new Schema({
+
+    /**
+     * The name of the user.
+     * @type {string}
+     * @required
+     */
     name: {
         type: String,
         required: true
     },
+
+    /**
+     * The email address of the user.
+     * @type {string}
+     * @required
+     * @unique
+     */
     email: {
         type: String,
         required: true,
         unique: true
     },
+
+    /**
+     * The password of the user.
+     * @type {string}
+     * @required
+     */
     password: {
         type: String,
         required: true
     },
+
+    /**
+     * A flag indicating whether the user is an admin.
+     * @type {boolean}
+     * @required
+     */
     isAdmin: {
         type: Boolean,
         required: true

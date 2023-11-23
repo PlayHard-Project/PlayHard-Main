9fe4b66a587e85403223a766c00de7dd4d00783d
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-11-23 01:44:06 -0400

    feat: Adding validations

--- src/Views/Pages/Contact.jsx
+++ src/Views/Pages/Contact.jsx
@@ -5,14 +5,46 @@ const Contact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [warnings, setWarnings] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Limpiar advertencias previas
+    setWarnings({
+      name: "",
+      email: "",
+      message: "",
+    });
+
+    // Validaciones
+    if (name.length < 5 || name.length > 50) {
+      setWarnings((prev) => ({ ...prev, name: "Name should be between 5 and 50 characters." }));
+    }
+
+    if (!email.match(/\S+@\S+\.\S+/)) {
+      setWarnings((prev) => ({ ...prev, email: "Invalid email format. It should be like example@gmail.com" }));
+    }
+
+    if (message.length < 10 || message.length > 250) {
+      setWarnings((prev) => ({ ...prev, message: "Message should be between 10 and 250 characters." }));
+    }
+
+    // Si hay advertencias, no continuar con el envío
+    if (Object.values(warnings).some((warning) => warning !== "")) {
+      return;
+    }
+
     // TODO: Handle form submission logic here
     console.log("Form submitted!");
     console.log("Name:", name);
     console.log("Email:", email);
     console.log("Message:", message);
+
     // Reset form fields
     setName("");
     setEmail("");
@@ -21,28 +53,50 @@ const Contact = () => {
 
   return (
     <div className="container">
-      <div className="sc-container">
-        <div className="text-container">
-          <label className="title">{title}</label>
-          <p>{description}</p>
-          <p>
-            Need to get in touch with us? Fill out the form with your inquiry or
-            find below the e-mail address jefer@gmail.com to contact us.
-          </p>
-          <Link to={"/home"}>
-            <button
-              className="sc-btn"
-              style={{ background: buttonColor }}
-            >
-              Back to HomePage
-            </button>
-          </Link>{" "}
-        </div>
-        <img src={imageSrc} alt={altText} />
+      <div className="contact-us">
+        <h1>Contact Us</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            {warnings.name && <p className="error-message">{warnings.name}</p>}
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            {warnings.email && <p className="error-message">{warnings.email}</p>}
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+            ></textarea>
+            {warnings.message && <p className="error-message">{warnings.message}</p>}
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
       </div>
-      <style>
-        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@800&display=swap');
-      </style>
     </div>
   );
 };

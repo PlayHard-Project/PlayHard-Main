e52c30e56d34869a489119d31d0cce9fc55994c8
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-11-23 09:01:12 -0400

    feat: Implement the page principal to display de orders

--- src/Views/PurchaseHistory/HistoryView.jsx
+++ src/Views/PurchaseHistory/HistoryView.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import PurchaseComponent from './PurchaseComponent';
 
-export default function 
-() {
+export default function ShoppingHistory() {
+  const [orders, setOrders] = useState([]);
+  const idUser = "1233";
+
+  useEffect(() => {
+    axios.get(`https://backend-fullapirest.onrender.com/orders/user/${idUser}`)
+      .then(response => {
+        setOrders(response.data);
+      })
+      .catch(error => {
+        console.error('Error al obtener las órdenes:', error);
+      });
+  }, [idUser]);
+
   return (
     <div>
+      <section>
         <h1>Shopping History</h1>
-        <h2>User Name: </h2>
-        <h2>User ID: </h2>
-    
+        <h2>User Name: {/* Add user name here */}</h2>
+        <h2>User ID: {idUser}</h2>
+      </section>
+      {orders.map(order => (
+        <PurchaseComponent key={order._id} idOrder={order._id} />
+      ))}
     </div>
-  )
+  );
 }

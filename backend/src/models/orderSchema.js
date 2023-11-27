d5987d6b5bda23e4e8689af01a17c2039894ae0e
Jefersson Coronel <135620350+JeferssonCL@users.noreply.github.com>
2023-11-27 00:49:44 -0400

    Update orderSchema.js

--- backend/src/models/orderSchema.js
+++ backend/src/models/orderSchema.js
@@ -82,7 +82,11 @@ const orderSchema = new Schema(
     },
     date: {
       type: Date,
-      default: () => new Date().toLocaleString("en-US", { timeZone: "America/La_Paz" }),
+      default: function () {
+        const currentDate = new Date();
+        currentDate.setUTCHours(0, 0, 0, 0);
+        return currentDate;
+      },
       required: true,
     }
   },

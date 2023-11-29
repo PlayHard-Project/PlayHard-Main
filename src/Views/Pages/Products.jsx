7bab0eb9d0d68704dec0882589466d4186b57b48
Jorge.cool <jorge.heredia.13036406@usalesiana.edu.bo>
2023-11-29 12:32:06 -0400

    CU-8686ff7rw View filters finished

--- src/Views/Pages/Products.jsx
+++ src/Views/Pages/Products.jsx
@@ -30,18 +30,18 @@ const Products = ({ setCartItemsQuantity, setSubTotal }) => {
   };
 
   const content = products.map(
-    ({ _id, imagePath, name, price, colorInformation, size }) => (
-      <ShoppingCard
-        _id={_id}
-        img={imagePath[0]}
-        title={name}
-        price={price}
-        colorInformation={colorInformation}
-        size={size}
-        setCartItemsQuantity={setCartItemsQuantity}
-        setSubTotal={setSubTotal}
-      />
-    )
+      ({ _id, imagePath, name, price, colorInformation, size }) => (
+          <ShoppingCard
+              _id={_id}
+              img={imagePath[0]}
+              title={name}
+              price={price}
+              colorInformation={colorInformation}
+              size={size}
+              setCartItemsQuantity={setCartItemsQuantity}
+              setSubTotal={setSubTotal}
+          />
+      )
   );
 
   console.log("Page ",{page})
@@ -49,25 +49,25 @@ const Products = ({ setCartItemsQuantity, setSubTotal }) => {
   console.log("Products:",{products})
 
   /**
- * Renders a loading component when there are no products available.
- * @param {Array} products - The array of products to be checked for emptiness.
- * @returns {JSX.Element} - The JSX representing the loading component.
- */
-if (products.length === 0) {
-  return (
-    <div
-      className="flex flex-col items-center justify-center p-3 gap-16 min-h-screen"
-    >
-      <GridLoader color="#023fc5" />
-    </div>
-  );
-}
+   * Renders a loading component when there are no products available.
+   * @param {Array} products - The array of products to be checked for emptiness.
+   * @returns {JSX.Element} - The JSX representing the loading component.
+   */
+  if (products.length === 0) {
+    return (
+        <div
+            className="flex flex-col items-center justify-center p-3 gap-16 min-h-screen"
+        >
+          <GridLoader color="#023fc5" />
+        </div>
+    );
+  }
 
   return (
-    <div className="container-product container" >
-      <Sidebar />
-      <CardsContainer content={content} />
-    </div>
+      <div className="container-product container" >
+        <Sidebar className="sidebar" />
+        <CardsContainer className="cards-container" content={content} />
+      </div>
   );
 };
 

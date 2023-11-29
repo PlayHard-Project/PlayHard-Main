f4f8280ad24fa50fc469e967a54c0603d1d40425
aguirrekarina <karina.aguirre.9491656@usalesiana.edu.bo>
2023-11-28 22:20:40 -0400

    finish view

--- src/Views/Settings/SettingView.jsx
+++ src/Views/Settings/SettingView.jsx
@@ -5,6 +5,7 @@ import "../../css/SettingView.css";
 
 const SettingView = () => {
   const [isActive, setIsActive] = useState(false);
+  const [isActiveMarketing, setIsActiveMarketing] = useState(false);
   const image =
     "https://res.cloudinary.com/playhardimages/image/upload/v1701149379/sjqmzsjnhdsovluojlgw.png";
 
@@ -12,13 +13,17 @@ const SettingView = () => {
     setIsActive(!isActive);
   };
 
+  const handleToggleMarketingEmail = () => {
+    setIsActiveMarketing(!isActiveMarketing);
+  };
+
   return (
-    <div className="container">
+    <div className="container md:flex" style={{height:'930px'}}>
       <div className="container-settings">
         <div className="text-container">
           <div className="setting-title">Settings</div>
           <div className="setting-item">
-            <span>Sent invoice to email: </span>
+            <span className="span-style">Sent invoice to email: </span>
             <button
               onClick={handleToggle}
               className={`toggle-button ${isActive ? "active" : "inactive"}`}
@@ -30,8 +35,26 @@ const SettingView = () => {
               )}
             </button>
           </div>
+
+          <div className="setting-item">
+            <span className="span-style">Receive marketing emails: </span>
+            <button
+              onClick={handleToggleMarketingEmail}
+              className={`toggle-button ${
+                isActiveMarketing ? "active" : "inactive"
+              }`}
+            >
+              {isActiveMarketing ? (
+                <MdToggleOn className="toggle-icon" />
+              ) : (
+                <MdToggleOff className="toggle-icon" />
+              )}
+            </button>
+          </div>
         </div>
-        <img src={image} alt="image" className="centered-image" />
+        <div className="hidden lg:block">
+          <img src={image} alt="image" className="centered-image" />
+        </div>
       </div>
     </div>
   );
